da74baf56b55c18feee6ac1754918a4667cc39d1
kbo831 <76991507+kbo831@users.noreply.github.com>
2025-11-25 00:58:53 +0900

    메인 반응형 , 스크립트 작업
    메인 컨텐츠 반응형, 애니메이션 스크립트 수정완료

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,66 +1,67 @@
 $(document).ready(function () {
-  //DOM 트리 구조가 완성되었을 때 실행됨(이미지,,폰트,비디오 무시, html 구조만 파싱되면 실행)
-  //gsap 플러그인등록
-  gsap.registerPlugin(ScrollTrigger, SplitText);
-
-  //전역 기본으로 설정할 효과
-  gsap.defaults({ ease: "back.out(1.4)" });
-
-  //타임라인 생성 (순서 제어용)
-  const tl = gsap.timeline();
-
+  gsap.registerPlugin(SplitText);
+  gsap.defaults({ ease: "power3.out" });
 
   $(window).on("load", function () {
-    // 이미지, 폰트, 외부 리소스까지 전부 로드된 후 실행됨
-      tl.add(() => {
-      //텍스트 애니메이션 실행
+    gsap.to(".intro-con", {
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      stagger: 0.1,
+      onComplete: () => {
+        // split + set
+        const splitText = SplitText.create(".intro-txt", { type: "chars" });
+        gsap.set(splitText.chars, { autoAlpha: 0, yPercent: -200 });
 
-      //splitText 대상지정 생성
-      const splitText = SplitText.create(".intro-txt", { type: "chars" });
+        // chars 등장
+        gsap.to(splitText.chars, {
+          yPercent: 0,
+          autoAlpha: 1,
+          duration: 0.8,
+          stagger: 0.02,
+          onComplete: () => {
+            const point = document.querySelector(".intro-txt.second .point");
+            point?.classList.add("active");
 
-      //애니메이션 실행 전 텍스트 상태 초기값 설정
-      gsap.set(splitText.chars, {
-        autoAlpha: 0, // opacity :0 , visibility:hidden
-        transformOrigin: "center center",
-        yPercent: -200,
-      });
 
-      // gsap 애니메이션 전에 active 클래스 추가, intro-con 등장
-      document.querySelector(".intro")?.classList.add("active");
+            // 30초 뒤 intro-con 숨기기
+            gsap.delayedCall(1, () => {
 
-      //gsap 애니메이션 설정
-      tl.to(splitText.chars, {
-        yPercent: 0,
-        autoAlpha: 1, // opacity :1 , visibility:visible
-        opacity: 1,
-        ease: "power3.out",
-        duration: .8,
-        stagger: {
-          each: 0.03,
-        },
-      });
+              const tl = gsap.timeline(); // 순서 제어용 타임라인 생성
 
-      tl.add(() => {
-        // 텍스트 분해 복원
-        splitText.revert();
-      });
+              tl.to(".intro-con", { opacity: 0, duration: 1, ease: "power1.out" })
+              .fromTo(".skip-card",
+                { opacity: 0, y: 100 },   // 시작 위치
+                { opacity: 1, visibility: "visible", y: 0, duration: 1, ease: "power3.out",
+                  onComplete: () => {
+                      console.log("skip-card 애니메이션 완료!");
+                    if (window.innerWidth > 1100) { // 윈도우 화면 1100px 이상에서만 
+                        const gap = 30;  // 카드 간격(px)
+                        const cards = gsap.utils.toArray(".skip-card .link");
+                        const center = 50; // 가운데 (%)
 
-      tl.add(() => {
-        // 복원된 구조에 스케일적용할 텍스트 클래스 추가
-        document
-          .querySelector(".intro-txt.second .point")
-          ?.classList.add("active");
-      })
-        .add(() => {
-          document.querySelector(".sc-con.intro-con")?.classList.add("hide");
-        }, "+=1");
+                        cards.forEach((card, i) => {
+                          const offset = (i - 1) * gap; 
+                          gsap.to(card, {
+                            left: `calc(${center}% + ${offset}%)`,
+                            duration: 1,
+                            ease: "power3.out"
+                          });
+                        });
+                    }
+                  }
+                 } // 마지막위치 
+              );
 
-      //end
+            });
+          }
+        });
+
+     
+        
+       
+
+      }
     });
-    //load end
   });
-
-
-
-
 });
