eb041bf9955b67e70e7dd43c21dbb01597face9a
kbo831 <76991507+kbo831@users.noreply.github.com>
2025-11-28 23:30:56 +0900

    Add files via upload
    브라우저 오픈 시, intro-con css 를 opacity:0, visibility : hidden 처리 하여  html 돔요소와 css가 먼저 파싱되어 애니메이션 대상을 안보임 처리하고 
    gsap 가 실행 되었을때 onStart 함수를 실행하여 intro-con css를 opacity :1 visibility : visible 하여 보이게 해야 
    브라우저 오픈 시 기존 텍스트가  먼저 나오는 현상을 방지할수 있음

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,8 +1,9 @@
-$(document).ready(function () {
-  gsap.registerPlugin(SplitText);
-  gsap.defaults({ ease: "power3.out" });
+$(document).ready(function () {//DOM 구조가 준비되면 호출
 
-  $(window).on("load", function () {
+  gsap.registerPlugin(SplitText); //텍스트 애니메이션 플러그인 등록
+  gsap.defaults({ ease: "power3.out" });//GSAP 애니메이션 기본실행 속도
+
+  $(window).on("load", function () { // HTML,CSS,IMG, VIDEO등 요소가 전부 파싱 된 다음 코드 실행
 
        //spreadCard 
         function spreadCard(){
@@ -24,20 +25,21 @@ $(document).ready(function () {
 
 
 
-        // split + set
-        const splitText = SplitText.create(".intro-txt", { type: "chars" });
-        gsap.set(splitText.chars, {
+        // splitText
+        const splitText = SplitText.create(".intro-txt", { type: "chars" }); // 분리할 텍스트 대상 지정
+        gsap.set(splitText.chars, { // 텍스트 기본 스타일 설정
             autoAlpha: 0,   // opacity 0 + visibility hidden
             yPercent: -100,   // 아래서 올라오는 효과를 위해 위치 조정
         });
 
         // chars 등장
-        gsap.to(splitText.chars, {
-          yPercent: 0,
-          autoAlpha:1,//opacity + visibility를 동시에 조절하는 GSAP 속성
+        gsap.to(splitText.chars, { // 
+          autoAlpha:1,//opacity 1 + visibility visible
+          yPercent: 0, //원래자리로
           duration: 0.5, // 애니메이션이 발생되는 시간
-          stagger: 0.04, //요소를 순서대로 조금씩 시간 차를 두고 애니메이션 시키는 옵션
+          stagger: 0.04, //각 글자별 시간차 애니메이션 실행하는 옵션
           onStart: ()=>{
+            //숨겨둔 "intro-con" 함수 실행시 보이게
             gsap.set(".intro-con", { opacity:1,visibility: 'visible' }); 
           },
           onComplete: () => {
@@ -45,8 +47,8 @@ $(document).ready(function () {
             point?.classList.add("active");
 
 
-            // 30초 뒤 intro-con 숨기기
-            gsap.delayedCall(0.7, () => {
+            // 0.7초 뒤 intro-con 숨기기
+            gsap.delayedCall(0.7, () => { //일정 시간 뒤 함수 호출 : delayedCall
 
               const tl = gsap.timeline(); // 순서 제어용 타임라인 생성
 
